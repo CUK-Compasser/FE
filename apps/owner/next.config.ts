2a7dd38d813b2321157b199300455e26d7f41900
고민균 <97932282+skyblue1232@users.noreply.github.com>
2026-02-25 05:32:11 +0900

    chore/#7/typescript-config

--- apps/owner/next.config.ts
+++ apps/owner/next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  experimental: {
+    externalDir: true,
+  },
+  transpilePackages: [
+    "@compasser/design-system",
+    "@compasser/tailwind-config",
+  ],
 };
 
 export default nextConfig;
