157f799331eb2495b51f0c9a576a91d2cb9b9f4f
고민균 <97932282+skyblue1232@users.noreply.github.com>
2026-03-16 00:19:47 +0900

    fix/icons

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -1,5 +1,5 @@
 // 이 파일은 자동 생성 파일입니다. (직접 수정 금지)
-export { Icon } from "./components/icon";
+export { Icon } from "./components/Icon";
 export { IconSprite } from "./components/iconSprite";
 export { iconNames } from "./generated/iconNames";
 export type { IconName } from "./generated/iconNames";
