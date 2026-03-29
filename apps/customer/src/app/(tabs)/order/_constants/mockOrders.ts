04d9b919344f7483d4b2cc19c660c3452a2c1074
고민균 <97932282+skyblue1232@users.noreply.github.com>
2026-03-30 04:50:17 +0900

    chore/mock-data

--- apps/customer/src/app/(tabs)/order/_constants/mockOrders.ts
+++ apps/customer/src/app/(tabs)/order/_constants/mockOrders.ts
@@ -15,11 +15,25 @@ export const IN_PROGRESS_ORDERS: OrderItem[] = [
     totalPrice: "15,000원",
     pickupTime: "2/27 19:00 ~ 20:00",
   },
+  {
+    id: 3,
+    storeName: "별동네 베이커리카페 별내본점",
+    orderSummary: "랜덤박스 Level.1 3개",
+    totalPrice: "15,000원",
+    pickupTime: "2/27 19:00 ~ 20:00",
+  },
+  {
+    id: 4,
+    storeName: "별동네 베이커리카페 별내본점",
+    orderSummary: "랜덤박스 Level.1 3개",
+    totalPrice: "15,000원",
+    pickupTime: "2/27 19:00 ~ 20:00",
+  },
 ];
 
 export const DONE_ORDERS: OrderItem[] = [
   {
-    id: 3,
+    id: 5,
     storeName: "별동네 베이커리카페 별내본점",
     orderSummary: "랜덤박스 Level.1 1개",
     totalPrice: "5,000원",
